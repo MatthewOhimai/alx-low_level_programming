55a18d9a09ad6fa8caab253fccb54e1da92ea458
MatthewOhimai <matthew4ohimai15@gmail.com>
2023-07-20 00:14:32 +0100

    Tenny

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,14 @@
  * Return void
  */
 void print_alphabet_x10(void)
+
 {
 	char c;
 	int i = 0;
 
 	while (i <= 9)
 	{
-	for (c = 'a'; c<= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 
